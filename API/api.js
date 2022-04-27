89ef522e518f0046770311f9ee386e6f60a32b25
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-04-28 00:20:23 +0530

    coin details api added

--- API/api.js
+++ API/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const apiurl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
-const specificcoin = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&sparkline=true";
+const specificcoin = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&sparkline=true";
 
 export const fetchData = async() =>{
     return axios.get(`${apiurl}`);
 }
 
-export const fetchCoinData = async() =>{
-    return axios.get(`${specificcoin}`);
+export const fetchCoinData = async(coinid) =>{
+    return axios.get(`${specificcoin}?&ids=${coinid}`);
 }
--- screens/CoinDetails.js
+++ screens/CoinDetails.js
@@ -1,25 +1,39 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { fetchCoinData } from '../API/api';
+const CoinDetails = ({ route }) => {
+    const { coinid } = route.params;
+    const [data, setdata] = useState([]);
 
-const CoinDetails = ({route, navigation}) => {
+    const getResult = async () => {
+        const res = await fetchCoinData(coinid);
+        setdata(res.data[0]);
+    }
 
-    const { coinid } = route.params; 
-  return (
-    <View style={styles.container}> 
-      <View>
-          
-      </View>
-      <Text>{coinid}</Text>
-    </View>
-  )
+    useEffect(() => {
+        getResult();
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.uppart}>
+            <Text>{data.total_volume}</Text>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-      backgroundColor: '#181631',
-      flex: 1
+        backgroundColor: '#181631',
+        flex: 1
     },
-  }
-  )
+    uppart: {
+        width: '100%',
+        height: 80,
+        backgroundColor: "white"
+    }
+}
+)
 
 export default CoinDetails
